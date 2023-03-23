223ef43156b1d45981101e213adc144d7b2cdcd3
Julia Isotalo <julia.isotalo@edu.sasky.fi>
2023-03-23 19:25:15 +0200

    Toteutetaan istunnon aloitus

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+    // Aloitetaan istunnot.
+    session_start();
+
     // Suoritetaan projektin alustusskripti.
     require_once '../src/init.php';
 
@@ -48,7 +51,8 @@
           if (isset($_POST['laheta'])) {
           require_once CONTROLLER_DIR . 'kirjaudu.php';
             if (tarkistaKirjautuminen($_POST['email'],$_POST['salasana'])) {
-              echo "Kirjautuminen ok!";
+            $_SESSION['user'] = $_POST['email'];
+            header("Location: " . $config['urls']['baseUrl']);
             } else {
               echo $templates->render('kirjaudu', [ 'error' => ['virhe' => 'Väärä käyttäjätunnus tai salasana!']]);
             }
