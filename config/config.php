cf4649c3e14b7a9cccc9c5fd53dabd03cca06f8a
Julia Isotalo <julia.isotalo@edu.sasky.fi>
2023-03-23 18:46:10 +0200

    Korjaus edelliseen

--- config/config.php
+++ config/config.php
@@ -18,10 +18,5 @@
   define("MODEL_DIR", PROJECT_ROOT . "src/model/");
   define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
   define("BASEURL", $config['urls']['baseUrl']);
-  define("PROJECT_ROOT", dirname(__DIR__) . "/");
-  define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
-  define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
-  define("MODEL_DIR", PROJECT_ROOT . "src/model/");
-  define("CONTROLLER_DIR", PROJECT_ROOT . "src/controller/");
 
 ?>
