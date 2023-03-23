7804eb720ab1c438825b2c31076b5a7c761851f0
Julia Isotalo <julia.isotalo@edu.sasky.fi>
2023-03-23 15:21:56 +0200

    Linkitykset etuvisulle ja tapahtuma-sivuille

--- src/view/template.php
+++ src/view/template.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="fi">
   <head>
-    <title>lanify - <?=$this->e($title)?></title>
+    <title>nimitähän - <?=$this->e($title)?></title>
     <meta charset="UTF-8">    
   </head>
   <body>
     <header>
-      <h1>lanify</h1>
+      <h1><a href="<?=BASEURL?>">nimitähän</a></h1>
     </header>
     <section>
       <?=$this->section('content')?>
     </section>
     <footer>
       <hr>
-      <div>lanify by Kurpitsa</div>
+      <div>Nimitähän by Kurpitsa</div>
     </footer>
   </body>
 </html>
