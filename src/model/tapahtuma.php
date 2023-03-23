7276ca0a043a2dbdecff2ee809e3b36d98952dfa
Julia Isotalo <julia.isotalo@edu.sasky.fi>
2023-03-23 16:41:42 +0200

    Tietojen päivityksiä

--- src/model/tapahtuma.php
+++ src/model/tapahtuma.php
@@ -3,10 +3,10 @@
   require_once HELPERS_DIR . 'DB.php';
 
   function haeTapahtumat() {
-    return DB::run('SELECT * FROM treenit ORDER BY tap_alkaa;')->fetchAll();
+    return DB::run('SELECT * FROM caravan ORDER BY tap_alkaa;')->fetchAll();
   }
   function haeTapahtuma($id) {
-    return DB::run('SELECT * FROM treenit WHERE idtapahtuma = ?;',[$id])->fetch();
+    return DB::run('SELECT * FROM caravan WHERE idtapahtuma = ?;',[$id])->fetch();
   }
 
 ?>
--- src/view/template.php
+++ src/view/template.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html lang="fi">
   <head>
-    <title>nimitähän - <?=$this->e($title)?></title>
+    <title>Karavaanarit ry - <?=$this->e($title)?></title>
     <meta charset="UTF-8"> 
     <link href="styles/styles.css" rel="stylesheet">   
   </head>
   <body>
     <header>
-      <h1><a href="<?=BASEURL?>">nimitähän</a></h1>
+      <h1><a href="<?=BASEURL?>">Karavaanarit ry</a></h1>
     </header>
     <section>
       <?=$this->section('content')?>
     </section>
     <footer>
       <hr>
-      <div>Nimitähän by Kurpitsa</div>
+      <div>Karavaanarit ry by Kurpitsa</div>
     </footer>
   </body>
 </html>
