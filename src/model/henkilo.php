4df75e7b77ccc188569dc65d2fad8969eeeb856b
Julia Isotalo <julia.isotalo@edu.sasky.fi>
2023-03-24 13:31:44 +0200

    Vaihtoavaimen lisääminen jasen-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -18,5 +18,14 @@
   function vahvistaTili($avain) {
     return DB::run('UPDATE jasen SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
   }
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE jasen SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM jasen WHERE nollausavain = ?', [$avain])->fetch();
+  }
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE jasen SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
 
 ?>
